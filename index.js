b59db63de5c92a84c002bcfdd5ef144c1cc74ae8
Hendy Almeida <hendy.almeida@hotmail.com>
2021-03-31 10:45:30 -0300

    rota post /projects

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const app = express();
 
 // console.log(app);
 
-app.get('/', (request, response) => {
-    return response.json([
+app.get('/projects', (req, res) => {
+    return res.json([
         'Projeto 1',
         'Projeto 2'
     ]);
 });
+
+
+app.post('/projects', (req, res) => {
+    return res.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+})
 
 app.listen(3000, () => {
     console.log('Servidor rodando!');
