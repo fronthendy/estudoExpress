fe81e18eb8d716fd4ccaa5d547607e09ee3b197c
Hendy Almeida <hendy.almeida@hotmail.com>
2021-03-31 10:06:52 -0300

    server express

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
-
 const app = express();
 
-console.log(app);
+// console.log(app);
+
+app.get('/', (request, response) => {
+    response.send('Olá, Pessoal!');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando!');
+});
